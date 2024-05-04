d4d4000527b7bd2a21eb1d5b947be7069f78697c
Sid-Athos <sa.bennaceur@gmail.com>
2024-05-05 00:19:00 +0200

    PET-26 forgot the provider

--- petpals-middleware/src/main/java/com/petpals/bootstrap/interceptors/JacksonExceptionHandler.java
+++ petpals-middleware/src/main/java/com/petpals/bootstrap/interceptors/JacksonExceptionHandler.java
@@ -3,8 +3,10 @@ package com.petpals.bootstrap.interceptors;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 import org.apache.http.HttpStatus;
 
+@Provider
 public class JacksonExceptionHandler implements ExceptionMapper<UnrecognizedPropertyException> {
 	@Override
 	public Response toResponse(UnrecognizedPropertyException e) {
